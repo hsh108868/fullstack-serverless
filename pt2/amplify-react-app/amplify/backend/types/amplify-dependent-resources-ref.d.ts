9019ff1c1f35be1b95b86ccdfb1980e3da49f6e1
hsh108868 <pusrty@gmail.com>
2022-01-25 17:36:06 +0900

    async-asign

---
+++ pt2/amplify-react-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,17 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "cryptofuntion": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "cryptoapi": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
